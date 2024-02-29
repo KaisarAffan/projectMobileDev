378a6062a2c4a38c9a3a0a8f39a2269048854271
KasiarAffan <kaisaraffan1@gmail.com>
2024-02-29 08:49:38 +0700

    fix adapter [Io]

--- app/src/main/java/com/example/appschool1/recylerViewAdapter.java
+++ app/src/main/java/com/example/appschool1/recylerViewAdapter.java
@@ -19,7 +19,6 @@ public class recylerViewAdapter extends RecyclerView.Adapter<recylerViewAdapter.
 
     Context context;
     List<Item> foods;
-
     public recylerViewAdapter(Context context, List<Item> foods) {
         this.context = context;
         this.foods = foods;
