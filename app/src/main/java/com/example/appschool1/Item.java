cf3493d3139fd83b5ddf0725a13dc60029017d50
KasiarAffan <kaisaraffan1@gmail.com>
2024-03-07 07:56:47 +0700

    fix image with URL [Io]

--- app/src/main/java/com/example/appschool1/Item.java
+++ app/src/main/java/com/example/appschool1/Item.java
@@ -3,15 +3,15 @@ package com.example.appschool1;
 public class Item {
     private String name;
     private String price;
-    private int image;
+    private String image;
 
-    public Item(String name, String price, int image) {
+    public Item(String name, String price, String image) {
         this.name = name;
         this.price = price;
         this.image = image;
     }
 
-    public int getImage() { return image; }
+    public String getImage() { return image; }
 
     public String getName() {
         return name;
--- app/src/main/java/com/example/appschool1/recylerViewAdapter.java
+++ app/src/main/java/com/example/appschool1/recylerViewAdapter.java
@@ -14,11 +14,14 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 
 public class recylerViewAdapter extends RecyclerView.Adapter<recylerViewAdapter.ViewHolder> {
 
     Context context;
     List<Item> foods;
+    private ItemClickListener mClickListener;
     public recylerViewAdapter(Context context, List<Item> foods) {
         this.context = context;
         this.foods = foods;
@@ -28,11 +31,12 @@ public class recylerViewAdapter extends RecyclerView.Adapter<recylerViewAdapter.
     @Override
     public recylerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_recyler_view,parent,false));
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull recylerViewAdapter.ViewHolder holder, int position) {
-        holder.foodImage.setImageResource(foods.get(position).getImage());
+        Glide.with(new View(context)).load(foods.get(position).getImage()).into(holder.foodImage);
         holder.foodName.setText(foods.get(position).getName());
         holder.foodPrice.setText(foods.get(position).getPrice());
     }
@@ -42,7 +46,7 @@ public class recylerViewAdapter extends RecyclerView.Adapter<recylerViewAdapter.
         return foods.size();
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView foodImage;
         TextView foodName,foodPrice;
 
@@ -51,8 +55,23 @@ public class recylerViewAdapter extends RecyclerView.Adapter<recylerViewAdapter.
             foodImage = itemView.findViewById(R.id.food_image);
             foodName = itemView.findViewById(R.id.food_name);
             foodPrice = itemView.findViewById(R.id.food_price);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
         }
     }
-
+    Item getItem(int id) {
+        return foods.get(id);
+    }
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
 
 }
